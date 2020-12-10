db7b78ef62999d8e09b12f410fb1bb45e880cc0f
Mateusz Duda <the_pro@wp.pl>
2020-12-10 20:54:29 +0100

    Day 10 with plus corrected

--- src/main/kotlin/com/c2v4/advent20/Day10Plus.kt
+++ src/main/kotlin/com/c2v4/advent20/Day10Plus.kt
@@ -3,20 +3,25 @@ package com.c2v4.advent20
 import arrow.syntax.function.memoize
 
 fun jolt2(input: String) =
-    input.split(splitRegex).map{it.toInt()}.sorted().let { arrangementsMemoized(it) }
+    input.split(splitRegex).map { it.toInt() }.sorted().let { arrangementsMemoized(it) }
 
-val arrangementsMemoized = {list:List<Int> -> arrangements(list)}.memoize()
-
-fun arrangements(input:List<Int>):Int{
-    if(input.size<2) return 1
-    val currentMax = input.max()!!
-    return setOf(1,2,3)
-        .map { currentMax - it }
-        .filter { input.contains(it) }
-        .map{ arrangements(input.subList(0,input.indexOf(it)+1))}
-        .sum()
+val arrangementsMemoized =
+    { list: List<Int> ->
+      var arrangements: (Int) -> Long = { it.toLong() }
+      arrangements =
+          { currentItem: Int ->
+                if (currentItem < 1) 1L
+                else
+                    setOf(1, 2, 3)
+                        .map { list[currentItem] - it }
+                        .filter { list.contains(it) || it == 0 }
+                        .map {
+                            arrangements(list.indexOf(it))
+                        }
+                        .sum()
+              }.memoize()
+      arrangements(list.size - 1)
     }
-
 
 
 fun main(args: Array<String>) {
